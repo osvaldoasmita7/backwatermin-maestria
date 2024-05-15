541509b65251f4ce4467f71b50b019e44865a094
Osvaldo Ponce <osvaldoasmita2019@outlook.com>
2024-05-15 16:43:24 -0600

    proyecto inicial

--- dist/app.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const server_1 = require("./models/server");
-require("dotenv").config();
-const server = new server_1.ServerServer();
-server.execute();
--- dist/connection/sequelizedb.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.sequelizeConn = void 0;
-const Sequelize = require("sequelize");
-exports.sequelizeConn = new Sequelize("waterdb", "root", "", {
-    host: "localhost",
-    dialect: "mysql",
-}, { timestamps: false });
